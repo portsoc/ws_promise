3172a96624b056900ddb8958bbce1897db95bdcf
Rich Boakes <rjb@port.ac.uk>
2017-03-24 11:38:17 +0000

    Update _00_commonerror_50.js
    brevity

--- _00_commonerror/_00_commonerror_50.js
+++ _00_commonerror/_00_commonerror_50.js
@@ -1,14 +1,11 @@
-var myLoadedData;
+let myLoadedData;
 
 function doSomethingMuchLater() {
   console.log("Much Later...", myLoadedData);
 }
 
 // load the file content
-fetch("example.json").then(
-  (r) => myLoadedData = r.json()
-)
+fetch("example.json").then( r => myLoadedData = r.json() );
 
-// set a timer so it's loaded ages before
-// it is needed
+// set a timer so it's loaded ages before it is needed
 setTimeout( doSomethingMuchLater, 3000);
